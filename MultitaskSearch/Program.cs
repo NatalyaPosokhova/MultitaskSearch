34f625c9a125ce2fcc6159fb3f34ebc109e12146
NatalyaPosokhova <natalya.posokhova@gmail.com>
2021-04-17 14:06:23 +0500

    Initialize project

---
+++ MultitaskSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultitaskSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
