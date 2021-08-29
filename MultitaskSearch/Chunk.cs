d388627c448814da2ffa85340185f1b45372756d
valerifc <valerifc2@gmail.com>
2021-08-29 15:17:14 +0500

    Delete IDataProvider

--- MultitaskSearch/Chunk.cs
+++ MultitaskSearch/Chunk.cs
@@ -4,12 +4,6 @@ using System.Text;
 
 namespace MultitaskSearch
 {
-    public interface IDataProvider
-    {
-        public Chunk GetData(int blockSize);
-        public bool IsDataExists();
-    }
-
     public struct Chunk
     {
         public int StartIndex;
