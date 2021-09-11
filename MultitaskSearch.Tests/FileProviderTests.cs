362964d2de5ae57619bac7252e033d3c239e221d
Natalya Posokhova <natalya.posokhova@gmail.com>
2021-09-11 18:23:29 +0500

    Add simple file provider test.

---
+++ MultitaskSearch.Tests/FileProviderTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MultitaskSearch.Tests
+{
+    public class FileProviderTests
+    {//Написать приватный метод, который создаёт временный файл, в конце удалить
+        string filePath = "../../../testProvider.txt";
+        [TearDown]
+        public void CleanUp()
+        {
+            File.Delete(filePath);
+        }
+        [Test]
+        public void ReadSimpleDataShouldBeSuccess()
+        {
+            //arrange
+            string expectedContent = "testad asdasdasdsa asdasdasd";
+            int expectedStartIndex = 0;
+            int dataSize = 30;
+
+            CreateTestData(expectedContent);
+            FileProvider fileProvider = new FileProvider(filePath, dataSize);
+
+            //act
+            List<Chunk> actualChunks = new List<Chunk>();
+            do
+            {
+                actualChunks.Add(fileProvider.Current);
+
+            } while (fileProvider.MoveNext());
+
+            //assert
+            Assert.AreEqual(expectedContent, actualChunks[0].Content);
+            Assert.AreEqual(expectedStartIndex, actualChunks[0].StartIndex);
+        }
+
+        private void CreateTestData(string source)
+        {
+            File.WriteAllText(filePath, source);
+        }
+    }
+}
---
+++ MultitaskSearch/FileProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultitaskSearch
+{
+    public class FileProvider : IEnumerable<Chunk>, IEnumerator<Chunk>
+    {
+        private readonly string _filePath;
+        private readonly int _dataSize;
+        public FileProvider(string filePath, int dataSize)
+        {
+            _filePath = filePath;
+            _dataSize = dataSize;
+        }
+        public Chunk Current => throw new NotImplementedException();
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Chunk> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
