f269899e4360a765a8bddab43da57eb3facdb16a
Natalya Posokhova <natalya.posokhova@gmail.com>
2021-09-12 14:11:37 +0500

    Start dividing on chunks method realization

--- MultitaskSearch/FileProvider.cs
+++ MultitaskSearch/FileProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace MultitaskSearch
@@ -9,10 +10,36 @@ namespace MultitaskSearch
     {
         private readonly string _filePath;
         private readonly int _dataSize;
+
         public FileProvider(string filePath, int dataSize)
         {
             _filePath = filePath;
             _dataSize = dataSize;
+        }
+
+        private IEnumerable<Chunk> CalculateChunks()
+        {
+            IEnumerable<Chunk> chunks = new List<Chunk>();
+            try
+            {
+                string text = File.ReadAllText(_filePath);
+                int startIndex = 0;
+                do
+                {
+                    Chunk chunk = new Chunk();
+                    char[] content = new char[] { };
+                    for (int i = startIndex; i < _dataSize || i < text.Length; i++)
+                    {
+                      
+                    }
+                } while (!text.Contains(string.Empty));
+
+            }
+            catch(Exception ex)
+            {
+                throw new FileNotFoundException(ex + ": cannot read data from file " + _filePath);
+            }
+            return chunks;
         }
         public Chunk Current => throw new NotImplementedException();
 
