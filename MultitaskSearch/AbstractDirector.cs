05f4836b85f79be389d64389b07f285c12382d51
Natalya Posokhova <natalya.posokhova@gmail.com>
2021-10-02 13:22:07 +0500

    Fix director

--- MultitaskSearch/AbstractDirector.cs
+++ MultitaskSearch/AbstractDirector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,16 +18,17 @@ namespace MultitaskSearch
         }
 
         public abstract void StartSearcher(string[] searchWords, Chunk chunk, IntermediateQueue intermediateQueue);
-        public abstract Task<Dictionary<string, IList<int>>> CreateCollector(IntermediateQueue intermediateQueue);
+        public abstract Task<Dictionary<string, IList<int>>> CreateCollector(IntermediateQueue intermediateQueue, int countChunks);
         public async Task<Dictionary<string, IList<int>>> GetWordsPositions()
         {
             IntermediateQueue intermediateQueue = new IntermediateQueue();
+            int countChunks = _chunks.Count();
             foreach(var chunk in _chunks)
-            {
+            { 
                 StartSearcher(_searchWords, chunk, intermediateQueue);
             }
 
-            var collector = await CreateCollector(intermediateQueue);
+            var collector = await CreateCollector(intermediateQueue, countChunks);
             return collector;
         }
     }
