60926ad214e405cfdf6f5f24f2d08aea7341720a
Natalya Posokhova <natalya.posokhova@gmail.com>
2021-10-02 13:21:59 +0500

    Add collector tests

---
+++ MultitaskSearch.Tests/CollectorTests.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace MultitaskSearch.Tests
+{
+    public class CollectorTests
+    {
+        private IntermediateQueue intermediateQueue;
+        private Dictionary<string, List<int>> expected;
+
+        [SetUp]
+        public void Setup()
+        {
+            intermediateQueue = new IntermediateQueue();
+            expected = new Dictionary<string, List<int>>();
+        }
+
+        [Test]
+        public void CollectDataFromTwoQueuesShouldBeSuccess()
+        {
+            //arrange
+            string word1 = "test";
+            int position1 = 25;
+            string word2 = "test";
+            int position2 = 56;
+            string word3 = "word";
+            int position3 = 45;
+
+            expected.Add(word1, new List<int> { 25, 56 });
+            expected.Add(word3, new List<int> { 45 });
+
+            intermediateQueue.Put(word1, position1);
+            intermediateQueue.Put(word2, position2);
+            intermediateQueue.Put(word3, position3);
+
+            int countChunks = 2;
+            var collector = new Collector(intermediateQueue, countChunks);
+            intermediateQueue.DetachTask();
+            intermediateQueue.DetachTask();
+
+            //act
+            Dictionary<string, List<int>> actual = collector.GetWordsAndPositionsList();
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void QueueIsEmptyShouldReturnEmptyDictionary()
+        {
+            //arrange
+            int countChunks = 2;
+            var collector = new Collector(intermediateQueue, countChunks);
+            intermediateQueue.DetachTask();
+            intermediateQueue.DetachTask();
+
+            //act
+            Dictionary<string, List<int>> actual = collector.GetWordsAndPositionsList();
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void QueueContainsOneWordAndPositionShouldBeSuccess()
+        {
+            //arrange
+            string word1 = "test";
+            int position1 = 0;
+
+            expected.Add(word1, new List<int> { position1 });
+            intermediateQueue.Put(word1, position1);
+
+            int countChunks = 1;
+            var collector = new Collector(intermediateQueue, countChunks);
+            intermediateQueue.DetachTask();
+
+            //act
+            Dictionary<string, List<int>> actual = collector.GetWordsAndPositionsList();
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }        
+    }
+}
---
+++ MultitaskSearch/Collector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultitaskSearch
+{
+    public class Collector
+    {
+        private IntermediateQueue _intermediateQueue;
+        private readonly int _countChunks;
+        public Collector(IntermediateQueue intermediateQueue, int countChunks)
+        {
+            _intermediateQueue = intermediateQueue;
+            _countChunks = countChunks;
+        }
+
+        internal Dictionary<string, List<int>> GetWordsAndPositionsList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
